7509931ab6d7f130b6a49ecc584f0fe99f1b9ab2
hqos8c <48017960+hqos8c@users.noreply.github.com>
2019-03-05 19:07:01 +0100

    Add files via upload

--- elso hazi/int/main.cpp
+++ elso hazi/int/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 double integrate(int n,double b, double a ){
  
@@ -8,7 +8,7 @@ double integrate(int n,double b, double a ){
     double h = (b-a)/(n);
     double sum = 0.0;
 
-    for(int  j = 1; j < (n-1); j++)
+    for(int  j = 1; j < n; j++)
     {
         double x = a + j*h;
         sum += fv(x);
@@ -16,22 +16,15 @@ double integrate(int n,double b, double a ){
     
     double res  =(h/2)*( fv(a) +2*sum + fv(b) );
     
-    std::cout<< n << std::endl;
     std::cout.precision(16);
-    std::cout<< res <<std::endl; 
-    std::cout<< "real = 1.34638795680345037669816"<<std::endl;
+    std::cout<<  res <<std::endl; 
     return res;
 }
 
 int main(int, char**) {
     double a = -1.0;
     double b  = 3.0;
-    
-    for(int n = 1; n < 10000;n = n*10)
-    {
-        integrate(n,b,a);
-    }
-    
-
-
+    int n = 10000;
+    integrate(n,b,a);
+    std::cout<< "real = 1.34638795680345037669816"<<std::endl;
 }
