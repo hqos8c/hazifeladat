eb774ff3fb31dcb3ba1cbb86237556802633bcf9
hqos8c <48017960+hqos8c@users.noreply.github.com>
2019-03-05 15:37:42 +0100

    Add files via upload

---
+++ elso hazi/template/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Halprog/template
+# Build directory: D:/Halprog/template/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ elso hazi/template/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cmath>
+
+template< typename T,typename K,typename D>
+T func(K fv1,D fv2,T x0 ){
+
+    int n = 5;
+    for(int i = 0; i < n; i++)
+        {
+            x0 =x0-(fv1(x0)/fv2(x0));
+        }
+
+    return x0;
+}
+
+int main() {
+    double x1 =func([](double x){ return x*x - 612.0; },
+        [](double x){ return 2.0*x; }, 10.0);
+
+
+    std::cout << x1 <<std::endl;
+}
