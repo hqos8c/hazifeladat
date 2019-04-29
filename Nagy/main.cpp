4587b1da0d1b69c66f8b3fbe82f2a82003559cba
hqos8c <48017960+hqos8c@users.noreply.github.com>
2019-04-29 15:43:14 +0200

    Add files via upload
    Beolvasom stringbe, de még nincs értéket a számoknak, most jön majd az átalakítás, hogy tudjak velük számolni.
    //lehet, hogy mátrixokban kéne tárolnom, talán úgy könnyebb számolni vele

--- Nagy/main.cpp
+++ Nagy/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 
 int main() {
 
    auto err = [](auto str){ std::cout << "Error in : " << str << "\n"; std::exit(-1); };
 
-    std::ifstream input("data.txt");
-    if(input.is_open()){
+    std::ifstream input("data_2.txt");
+    if(input.is_open())
+    {
         std::cout<<"so far so good"<<std::endl;
+        
+        std::string str{std::istreambuf_iterator<char>(input),std::istreambuf_iterator<char>()};
+        /*std::cout<<str.size()<<std::endl;
+        std::cout<<str<<std::endl;*/
+
+
+
     }
     else{err("opening"); }
+
+
+
 }
