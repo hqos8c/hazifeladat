243d3782f3fe3429bccddd9eaef69643ff3c4d16
hqos8c <48017960+hqos8c@users.noreply.github.com>
2019-04-30 10:28:20 +0200

    Add files via upload
    végig tudok menni az elemeken és ellenőriztem is, hogy jól megy végig rajta, most már kezdődhet az adatok átalakítása és feldolgozása

--- Nagy/main.cpp
+++ Nagy/main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include<sstream>
+#include<variant>
+#include<cmath>
+#include<vector>
 
 int main() {
 
    auto err = [](auto str){ std::cout << "Error in : " << str << "\n"; std::exit(-1); };
+    struct Nope{};
 
     std::ifstream input("data_2.txt");
     if(input.is_open())
     {
         std::cout<<"so far so good"<<std::endl;
         
-        std::string str{std::istreambuf_iterator<char>(input),std::istreambuf_iterator<char>()};
-        /*std::cout<<str.size()<<std::endl;
-        std::cout<<str<<std::endl;*/
-
-
-
+        int sum =0;
+        std::string tmp_1;
+        std::string tmp_2;
+        for(std::string line;std::getline(input,tmp_1,';'); )
+        {
+            sum += 1;
+        } 
+        std::cout<<sum<<std::endl;
     }
     else{err("opening"); }
-
-
-
 }
