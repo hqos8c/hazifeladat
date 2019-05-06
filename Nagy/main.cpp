6eff2ab4b01313ede96c6856aa0e8be301980b6a
hqos8c <48017960+hqos8c@users.noreply.github.com>
2019-05-06 16:43:50 +0200

    Add files via upload

--- Nagy/main.cpp
+++ Nagy/main.cpp
@@ -7,23 +7,6 @@
 #include <vector>
 #include <utility>
 
-struct TemperatureData 
-{
-    std::string name;
-    double value;
-};
-
-struct TimeData 
-{
-    int time;
-    std::vector<TemperatureData> data;
-};
-
-struct DateData {
-    int date;
-    std::vector<TimeData> times;
-};
-
 std::pair<std::string,std::string> splitFirst(std::string input, char separator) {
     auto pos = input.find(separator);
 
@@ -44,7 +27,7 @@ std::vector<std::string> split(std::string input, char separator) {
     std::vector<std::string> result = std::vector<std::string>();
     std::string remain = input;
     while(!remain.empty()){
-        auto pair = splitFirst(remain, separator);
+        std::pair<std::string,std::string> pair = splitFirst(remain, separator);
         result.push_back(pair.first);
         remain = pair.second;
     }
@@ -52,7 +35,7 @@ std::vector<std::string> split(std::string input, char separator) {
 }
 
 std::pair<std::string,double> parseData(std::string row) {
-    auto splitted = splitFirst(row, ',');
+    std::pair<std::string,std::string> splitted = splitFirst(row, ',');
     return std::pair<std::string, double>(splitted.first, std::stod(splitted.second));
 }
 
@@ -60,26 +43,33 @@ bool isDigit(char c) {
     return c >= '0' && c <= '9';
 }
 
-std::variant<> parseRow(std::string row) {   
+std::vector<double> alldata(std::pair<std::string,double> data,std::vector<double> place){
+
+}
+
+
+std::pair<std::string,double> parseRow(std::string row) {   
     if (isDigit(row[0])) {
-        auto r = split(row, ' ');
-        auto year = r[0];
-        auto month = r[1];
-        auto day = r[2];        
-        auto data = parseData(r[r.size() - 1]);
+        std::vector<std::string> r = split(row, ' ');
+        int year = std::stoi(r[0]);
+        int month = std::stoi(r[1]);
+        int day = std::stoi(r[2]);
+        std::pair<std::string,double> data = parseData(r[r.size() - 1]);
+        return data;
     } else {
-        auto data = parseData(row);
+        std::pair<std::string,double> data = parseData(row);
+    return data;
     }
 }
- 
 
 int main() {
-
     std::ifstream input("data_2.txt");
     if(input.is_open()) { 
         std::string line;
+        int ref = 0;
         while( std::getline(input, line, ';') ) {
-            parseRow(line);
+            std::pair<std::string,double> data = parseRow(line);
+            
         }
     }
 }
