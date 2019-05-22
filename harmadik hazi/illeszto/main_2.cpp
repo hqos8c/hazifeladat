e63426f95659aabf80fc832851c45d244a398f5f
hqos8c <48017960+hqos8c@users.noreply.github.com>
2019-05-22 19:21:27 +0200

    Add files via upload

--- harmadik hazi/illeszto/main_2.cpp
+++ harmadik hazi/illeszto/main_2.cpp
@@ -1,61 +1,68 @@
 #include <iostream>
-#include<cmath>
-#include<vector>
-#include<numeric>
-#include<array>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <array>
 #include <future>
+#include <execution>
+
 
 std::pair<double,double> frame (std::vector<double> const& X,std::vector<double> const& Y){
-    double xsum = (accumulate(X.begin(),X.end(),0.0)/ X.size());
-    double ysum = (accumulate(Y.begin(),Y.end(),0.0)/ Y.size());
+
+    auto zero = [](double X, double Y){return 0.0;};
     auto sq = [] (double x){return x*x;};
+    double xsum = 0.0;
+    double ysum = 0.0;
+    
+    auto sums = [&xsum,&ysum](double X, double Y){
+        xsum += X;
+        ysum += Y;
+        return 0.0;
+    };
 
-// az xsum és az ysum kell, őket nem akarjuk újra és újra kiszámolni
-// ezért nem tettem őket bele a párhuzamosításba
+    std::inner_product(X.begin(),X.end(),Y.begin(),0.0,zero,sums);
+    xsum/=X.size();
+    ysum/=Y.size();
 
-    auto mylambda = [xsum,ysum ,sq](std::vector<double> X,std::vector<double> Y,int i){
-        double up =(X[i]-xsum)*(Y[i]-ysum);
-        double down = sq(X[i]-xsum);
-        std::pair<double,double> par={up, down};
-        return par;
+    double b_up = 0.0;
+    double b_down=0.0;
+   
+    auto lambda = [xsum,ysum,sq,&b_up,&b_down](double X,double Y) {
+        b_up += (X-xsum) * (Y-ysum);
+        b_down += sq(X-xsum);
+        return 0.0;
         };
 
+    std::inner_product(X.begin(),X.end(),Y.begin(),0.0,zero,lambda);
 
-//szálkezelés kezdete
-        auto t1=std::async(mylambda,X,Y,0);
-        auto t2=std::async(mylambda,X,Y,1);
-        auto t3 = std::async(mylambda,X,Y,2);
-        std::vector< std::pair<double,double> > vec(3);
-        vec[0]=t1.get();
-        vec[1]=t2.get();
-        vec[2]=t3.get();
-// szálkezelés vége
-
-    double up=0.0;
-    double down=0.0;
-    for(auto e:vec){up +=e.first;}
-    for(auto e:vec){down += e.second;}
-//szálak adatainak kinyerése
-        
-        if(std::abs(down)<0.000000001){
-            std::cout<<"Mathematical problem detected"<<std::endl;
-            exit(-1);
-        }
-        else{
-        double m = up/down;
-        double b= ysum-m*xsum;
-        return {b,m};
-        }
+    if(b_down == 0.0)
+    {
+        std::cout<<"mathematical problem detected"<<std::endl;
+        exit(-1);
     }
+    
+    else
+    {
+        double b = b_up/b_down;
+        double m = ysum - b*xsum;
+
+        std::cout<< b<< std::endl;
+        std::cout<<m<<std::endl;
+
+        return {b, m};
+    }
+}
+
 int main() {
 
 std::vector<double> X ={3.0,5.0,7.0};
 std::vector<double> Y ={9.0,11.0,13.0};
 
-std::pair<double,double> data = frame(X,Y);
+frame(X,Y);
 double ref_b = 1;
 double ref_m =6; 
-std::cout<<"ref b is";std::cout<<' '<< ref_b<<std::endl;
-std::cout<< "ref m is";std::cout<<' '<<ref_m<< std::endl;
-std::cout<<data.second<<" the real b is"<<std::endl<<data.first<<" the real m is"<<std::endl;
+std::cout<<"ref b is";std::cout<<' ';
+std::cout<< ref_b<<std::endl;
+std::cout<< "ref m is";std::cout<<' ';
+std::cout<<ref_m<< std::endl;
 }
