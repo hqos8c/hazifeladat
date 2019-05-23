88183c65b135d8d151f603fed1fbdcacc9769cb1
hqos8c <48017960+hqos8c@users.noreply.github.com>
2019-05-23 13:24:32 +0200

    Add files via upload

--- harmadik hazi/illeszto/main_2.cpp
+++ harmadik hazi/illeszto/main_2.cpp
@@ -6,33 +6,35 @@
 
 std::pair<double,double> frame (std::vector<double> const& X,std::vector<double> const& Y){
 
-    auto zero = [](double X, double Y){return 0.0;};
+    std::vector<double> start={0.0,0.0};
+
+    auto second = [](std::vector<double>& v, std::vector<double> old){
+        old[0] += v[0];
+        old[1] += v[1];
+        return old;
+        };
     auto sq = [] (double x){return x*x;};
-    double xsum = 0.0;
-    double ysum = 0.0;
-    
-    auto sums = [&xsum,&ysum](double X, double Y){
-        xsum += X;
-        ysum += Y;
-        return 0.0;
+   
+    auto sums = [](double X, double Y){
+        std::vector<double> vector = {X,Y};
+        return vector;
     };
 
-    std::inner_product(X.begin(),X.end(),Y.begin(),0.0,zero,sums);
-    xsum/=X.size();
-    ysum/=Y.size();
+    std::vector<double> result = inner_product(X.begin(),X.end(),Y.begin(),start,second,sums);
+    double x_sum=result[0]/X.size();
+    double y_sum =result[1]/Y.size();
 
-    double b_up = 0.0;
-    double b_down=0.0;
-   
-    auto lambda = [xsum,ysum,sq,&b_up,&b_down](double X,double Y) {
-        b_up += (X-xsum) * (Y-ysum);
-        b_down += sq(X-xsum);
-        return 0.0;
+
+    auto lambda = [x_sum,y_sum,sq](double X,double Y) {
+        double b_up = (X-x_sum) * (Y-y_sum);
+        double b_down = sq(X-x_sum);
+        std::vector<double> v ={b_up,b_down};
+        return v;
         };
 
-    std::inner_product(X.begin(),X.end(),Y.begin(),0.0,zero,lambda);
+    result = std::inner_product(X.begin(),X.end(),Y.begin(),start,second,lambda);
 
-    if(b_down == 0.0)
+    if(std::abs(result[1]) < 0.0000000000001)
     {
         std::cout<<"mathematical problem detected"<<std::endl;
         exit(-1);
@@ -40,13 +42,12 @@ std::pair<double,double> frame (std::vector<double> const& X,std::vector<double>
     
     else
     {
-        double b = b_up/b_down;
-        double m = ysum - b*xsum;
-
+        double b = result[0]/result[1];
+        double m = y_sum - b*x_sum;
         std::cout<< b<< std::endl;
         std::cout<<m<<std::endl;
 
-        return {b, m};
+        return {b,m};
     }
 }
 
